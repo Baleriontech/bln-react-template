351e0948fdf171b1766295bef6e51ae49f288142
Krit <tanakrit@belerion.co.th>
2025-05-08 15:57:02 +0700

    :package: add git ignore

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,8 @@ export default function Header() {
   return (
     <header className="p-2 flex gap-2 bg-white text-black justify-between">
       <nav className="flex flex-row">
+        <div className="px-2 font-bold opacity-60"> BLN - React Template </div>
+        <div className="mx-2 border" />
         <div className="px-2 font-bold">
           <Link to="/">Home</Link>
         </div>
