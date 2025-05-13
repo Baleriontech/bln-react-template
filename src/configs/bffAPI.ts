0525e0e2455ef0397c3c1b0f155a4640ba7adca9
Tanaroeg O-Charoen <tanaroeg@balerion.co.th>
2025-05-13 16:25:30 +0700

    :art: add debug message on config

--- src/configs/bffAPI.ts
+++ src/configs/bffAPI.ts
@@ -1,5 +1,5 @@
 export const config = {
-  BASE_API_URL: import.meta.env.VITE_BASE_API_URL ?? undefined,
+  BASE_API_URL: import.meta.env.VITE_BASE_API_URL ?? 'BASE_API_URL NOT FOUND',
 } as const
 
 export default config
