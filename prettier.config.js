3ec4fcedd3c9a43f5029eb445300fb9696a97b4f
Tanaroeg O-Charoen <tanaroeg@balerion.co.th>
2025-05-13 17:06:25 +0700

    :wrench: improve prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,15 @@
 
 /** @type {import('prettier').Config} */
 const config = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  singleAttributePerLine: true,
+  jsxBracketSameLine: false,
+  bracketSameLine: false,
   semi: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
+  printWidth: 150,
 }
 
 export default config
