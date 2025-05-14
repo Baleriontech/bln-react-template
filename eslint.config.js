beeeb419717ca98ecacce53f2b5dcca536b1a5aa
Tanaroeg O-Charoen <tanaroeg@balerion.co.th>
2025-05-14 11:08:09 +0700

    :wrench: setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,56 @@
 //  @ts-check
 
-import { tanstackConfig } from '@tanstack/eslint-config'
+// eslint.config.js
+import js from '@eslint/js'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import prettierPlugin from 'eslint-plugin-prettier'
+import globals from 'globals'
+import prettierConfig from './prettier.config.js'
 
-export default [...tanstackConfig]
+export default [
+  js.configs.recommended,
+
+  {
+    name: 'typescript',
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: false,
+        },
+      ],
+      ...ts.configs.recommended.rules,
+    },
+  },
+
+  {
+    name: 'prettier-integration',
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': ['error', prettierConfig],
+    },
+  },
+]
